8e40f359359fd1e88627b028716effb3363f4083
Sercan <srsr141986@gmail.com>
2021-03-04 14:37:25 +0100

    First

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1, DailyPrice=100, ModelYear=2010, Description="Station Wagon"},
+                new Car{Id=2,BrandId=4,ColorId=4, DailyPrice=200, ModelYear=2020, Description="Sedan"},
+                new Car{Id=3,BrandId=3,ColorId=1, DailyPrice=300, ModelYear=2012, Description="Coupe"},
+                new Car{Id=4,BrandId=4,ColorId=3, DailyPrice=200, ModelYear=2010, Description="Station Wagon"},
+                new Car{Id=5,BrandId=5,ColorId=7, DailyPrice=500, ModelYear=2030, Description="Hybrid"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carDeleted = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carDeleted);
+
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Entities/Abstract/IEntity.cs
+++ Entities/Abstract/IEntity.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Concrete
+namespace Entities.Abstract
 {
-    class Product
+    public interface IEntity
     {
+
     }
 }
