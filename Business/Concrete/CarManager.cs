db5a14cb70b47e14d47d7d747bcf65ab574ced83
Sercan <srsr141986@gmail.com>
2021-03-10 14:56:45 +0100

    10.03

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,31 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Description.Length < 2 && car.DailyPrice<=0)
+            {
+                Console.WriteLine("Car name is too short");
+            }
+            else
+            {
+                _carDal.Add(car);
+            }
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
         }
     }
 }
